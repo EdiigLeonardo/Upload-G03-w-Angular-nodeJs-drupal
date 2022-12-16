c33f88f87f6f7e72a232c6516059e1738f5676de
EdiigLeonardo <65238422+EdiigLeonardo@users.noreply.github.com>
2022-12-16 13:52:48 +0000

    Fix Canais bug

---
+++ src/app/pages/canais/canais.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-canais',
+  templateUrl: './canais.component.html',
+  styleUrls: ['./canais.component.scss']
+})
+export class CanaisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
