df18c50ab34ad5db6ec20f2175f44423c0f22ec7
LuisMartins71 <109528943+LuisMartins71@users.noreply.github.com>
2023-01-10 07:30:28 +0000

    08 Jan(3)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UploadService} from "../../service/upload.service";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  Videos: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public service: UploadService ) { }
 
   ngOnInit(): void {
+    this.service.getVideos().subscribe((videos)=>{
+      this.Videos = videos;
+    })
   }
 
 }
