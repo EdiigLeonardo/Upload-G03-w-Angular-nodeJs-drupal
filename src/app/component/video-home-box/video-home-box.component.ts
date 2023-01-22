deb06228b5cf85f379c6cfa1925460388a00132a
EdiigLeonardo <65238422+EdiigLeonardo@users.noreply.github.com>
2023-01-22 22:40:53 +0000

    likes, comments, home, and too much things
    Lorem Ipsum

--- src/app/component/video-home-box/video-home-box.component.ts
+++ src/app/component/video-home-box/video-home-box.component.ts
@@ -4,6 +4,8 @@ import {faThumbsUp} from "@fortawesome/free-regular-svg-icons";
 import {faThumbsDown} from "@fortawesome/free-regular-svg-icons";
 import {faShareNodes} from "@fortawesome/free-solid-svg-icons";
 import {faBookmark as faBookmarkSolid} from "@fortawesome/free-solid-svg-icons";
+import {ActivatedRoute} from "@angular/router";
+import {UploadService} from "../../service/upload.service";
 
 @Component({
   selector: 'app-video-home-box',
@@ -11,12 +13,14 @@ import {faBookmark as faBookmarkSolid} from "@fortawesome/free-solid-svg-icons";
   styleUrls: ['./video-home-box.component.scss']
 })
 export class VideoHomeBoxComponent implements OnInit {
+  id = this.route.snapshot.params['id'];
 
   faThumbsUp = faThumbsUp;
   faThumbsDown = faThumbsDown;
   faBookmark = faBookmark;
   faBookmarkSolid = faBookmarkSolid;
   faShareNodes = faShareNodes;
+
   @Input() thumb = "";
   @Input() min_video_profile_photo = "";
   @Input() video_duration = "";
@@ -27,9 +31,16 @@ export class VideoHomeBoxComponent implements OnInit {
   @Input() canal = "";
   @Input() link_to_video = "";
 
+  toggleFavorite(){
+    this.service.toogleFavorite(this.id)
+  }
+
+  get favorite(){
+    return this.service.isFavorite(this.id);
+  }
 
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public service: UploadService) { }
 
   ngOnInit(): void {
   }
